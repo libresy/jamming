549cbf617731f353f111957f01f9f66a9270ccb9
Libresy <ymarmon@gmail.com>
2018-05-17 13:01:29 -0400

    Add files via upload
    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import PlayList from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Spotify from '../../util/Spotify';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: 'Yael', artist: 'Test1', album: 'Test2', id: '1234' },
+        { name: 'Yael2', artist: 'Test3', album: 'Test4', id: '12345'},
+        { name: 'Yael3', artist: 'Test5', album: 'Test6', id: '123456'}],
+      playlistName : 'Playlist 1',
+      playlistTracks: [
+      { name: 'Yael', artist: 'Test1', album: 'Test2', id: '1234' },
+      { name: 'Yael2', artist: 'Test3', album: 'Test4', id: '12345' },
+      { name: 'Yael3', artist: 'Test5', album: 'Test6', id: '123456' }]
+    };
+    /*  this.state = {
+      searchResults: [],
+      playlistName: 'Playlist',
+      playlistTracks: []
+     }; */
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+    Spotify.getAccessToken();
+  };
+ 
+  addTrack(track) {
+      
+      if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+        // this.state.playlistTracks.push(track);
+        let tracks = this.state.playlistTracks;
+        tracks.push(track);
+        //this.setState(this.setstate.playlistTracks);
+        this.setState({playlistTracks: tracks});
+      }
+    };
+
+    removeTrack(track){
+      let tracks = this.state.playlistTracks;
+      tracks = tracks.filter(playlistTrack => playlistTrack.id !== track.id);
+      this.setState({ playlistTracks: tracks });
+      //this.setState(this.state.playlistTracks);
+    };
+
+    updatePlaylistName(name){
+      this.setState({playlistName : name}); 
+    };
+    
+    savePlaylist() {
+      Spotify.getAccessToken();
+
+      const trackURIs = this.state.playlistTracks.map(track => track.uri);
+      Spotify.savePlaylist(this.state.playlistName, trackURIs)
+        this.setState ({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        });
+    };
+
+    search(term) {
+      Spotify.getAccessToken();
+
+      Spotify.search(term)
+        .then(tracks  => { 
+          this.setState({ searchResults: tracks  });
+      });
+      console.log(term);
+    };
+  
+
+  render() {
+    return (
+      <div>
+        {Spotify.getAccessToken()}
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+         <SearchBar onSearch={this.search}/>
+        <div className="App-playlist">
+        <SearchResults 
+          searchResults = {this.state.searchResults}
+          onAdd={this.addTrack} onRemove={this.removeTrack} />        
+        <PlayList 
+                  playlistName = {this.state.playlistName}
+                  tracks = {this.state.playlistTracks}
+                  onAdd={this.addTrack}
+                  onRemove={this.removeTrack} 
+                  onNameChange={this.updatePlaylistName}
+                  onSave={this.savePlaylist}
+                  onSearch={this.search}
+            />
+            
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
