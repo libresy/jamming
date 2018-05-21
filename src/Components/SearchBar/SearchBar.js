4418e112c8b32bd63331755063fbd55a78d0e54d
libresy <ymarmon@gmail.com>
2018-05-21 10:11:43 -0400

    keydown

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -9,22 +9,29 @@ class SearchBar extends React.Component {
         }
         this.search = this.search.bind(this)
         this.handleTermChange = this.handleTermChange.bind(this)
+        this.onKeyDown = this.onKeyDown.bind(this)
     }
 
     search() {
         this.props.onSearch(this.state.term)
-    }
+    };
 
     handleTermChange(event) {
         this.setState({ term: event.target.value })
-    }
+    };
+
+    onKeyDown(event) {
+        if (event.key === "Enter" || event.key === 13) {
+            this.props.onSearch(this.state.term);
+        }
+    };
 
     render() {
         return (
             <div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist"
                     onChange={this.handleTermChange}
-
+                    onKeyDown={this.onKeyDown}
                 />
                 <a onClick={this.search}>SEARCH</a>
             </div>
