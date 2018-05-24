06bc7154c6aa65c724de027a69c459088ae0f196
libresy <ymarmon@gmail.com>
2018-05-24 15:31:38 -0400

    Merge branch 'master' of https://github.com/libresy/jamming

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -20,7 +20,7 @@ class SearchBar extends React.Component {
         this.setState({ term: event.target.value })
     };
     
-    // I can't see this change on github on the site. I know it is checked in!
+    // can't see this change on github on the site. I know it is checked in!
     onKeyDown(event) {
         if (event.key === "Enter" || event.key === 13) {
             this.props.onSearch(this.state.term);
